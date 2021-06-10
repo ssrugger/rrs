2e3febecfdab6e0ff515d96d66de5e3c32aeef29
ssrugger <40933665+ssrugger@users.noreply.github.com>
2021-06-10 09:48:08 +0800

    Update ssh_key.sh

--- ssh_key.sh
+++ ssh_key.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 mkdir -p ~/.ssh
-curl -Ls https://raw.githubusercontent.com/ssrugger/rrs/master/authorized_keys >> ~/.ssh/authorized_keys
+curl -Ls https://raw.githubusercontent.com/ssrugger/rrs/master/GCP-Key >> ~/.ssh/authorized_keys
 echo -e "\033[32mINFO\033[0m Successfully added SSH key: GCP-Key"
+curl -Ls https://raw.githubusercontent.com/ssrugger/rrs/master/VNC-Key >> ~/.ssh/authorized_keys
+echo -e "\033[32mINFO\033[0m Successfully added SSH key: VNC-Key"
 sed -i 's/^#\?PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
 PasswordAuthentication=$(cat /etc/ssh/sshd_config | grep "PasswordAuthentication no" | awk '{print $2}')
 echo -e "\033[32mINFO\033[0m Password authentication: ${PasswordAuthentication}"
